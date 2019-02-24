c90e77eda7ff64ef9964dffda7833a53e602dffa
Syed Aziz <syedaziz27x@gmail.com>
2019-02-24 18:35:09 -0500

    updated vid component, have vid hardcoded for now

--- src/containers/Video.js
+++ src/containers/Video.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-const Video = ()=>{
-    return <h1>Video</h1>
+
+class Video extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: 'LH4Y1ZUUx2g',
+            link: `https://www.youtube.com/embed/LH4Y1ZUUx2g?autoplay=1&fs=1&origin=http://localhost:3000`,
+            data: {}
+        }
+    }
+
+    getData = () => {
+        axios({
+            method: 'get',
+            url: 'https://www.googleapis.com/youtube/v3/videos',
+            params: {
+                part: 'id,snippet,statistics',
+                key: 'AIzaSyDeTfhlCohwwrwgaOm4Hso37sclFReUkoY',
+                id: this.state.id,
+            }
+        })
+            .then((data) => {
+                console.log(data);
+                this.setState({ data: data.data.items[0].statistics }, () => console.log(this.state))
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    componentDidMount(props) {
+        this.getData()
+    }
+
+    render() {
+        return (
+            <>
+                <div className='row'>
+                    <iframe title='yt-video' type="text/html" width="640" height="360"
+                        src={this.state.link} frameBorder="0"></iframe>
+                </div>
+                <div className='row'>
+                    <div className='col'>Views: {this.state.data.viewCount}</div>
+                    <div className='col'>Likes: {this.state.data.likeCount}</div>
+                    <div className='col'>Dislikes: {this.state.data.dislikeCount}</div>
+                    <div className='col'>Comments: {this.state.data.commentCount}</div>
+                </div>
+            </>
+        );
+    }
 }
 
-export default Video
+export default withRouter(Video);
