257c849c3c0274c1dbf27434c32d6571c79de90b
nadavgree <nadavgreen@pursuit.org>
2019-02-24 15:27:14 -0500

    created home components

---
+++ src/components/Feedbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Feedbar = () => {
+    return <>
+    </>
+}
+
+export default Feedbar
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,50 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import {Link, withRouter} from 'react-router-dom'
+import Feedbar from '../components/Feedbar'
+import Feedlist from '../components/Feedlist'
+import Header from '../components/Header'
 
 class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: "Mo",
+            feed:["Lol","Girl","Boy"]
+        }
+    }
+
+    feedLoad = () => {
+        axios({
+            method: 'get',
+            url: 'https://www.googleapis.com/youtube/v3/videos',
+            params: {
+              part: 'id,snippet,statistics',
+              key: 'AIzaSyD1HDXH0JOccPKU7SYfh08ctspDqbUc4SI',
+              id: this.state.feed[0],
+            }
+          })
+        .then(()=>{
+
+        })
+        .catch(()=>{
+
+        })
+    }
+    
+    componentDidMount(){
+        this.feedLoad()
+    }
 
     render() {
       return (
         <>
-          <h1>Home</h1>
+          <Header name={this.state.name}/>
+          <Feedlist feed={this.state.feed}/>
+          <Feedbar />
         </>
       );
     }
   }
 
-export default Home
+export default withRouter(Home)
