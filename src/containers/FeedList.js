21bf58555c695658f7d0148d1f00bc22d63f3239
JayRodrig <mrjosemlrod@gmail.com>
2019-02-25 21:46:39 -0500

    added initial files, titles are showing on the feed component

--- src/containers/FeedList.js
+++ src/containers/FeedList.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import CreateListTitle from '../components/feed-list_components/create-list-title';
+import ExploreListFeed from '../components/feed-list_components/explore-list-title';
 
 const FeedList = ()=>{
-    return <h1>Feedlist</h1>
+    return(
+        <>
+        <div className='container'>
+            <div className='row'>
+                <CreateListTitle />
+                <ExploreListFeed />
+            </div>
+            <div className='row'>
+    
+            </div>
+        </div>
+    </>
+    )
 }
 
 export default FeedList;
