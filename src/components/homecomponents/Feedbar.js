a45adbe7c19d8ab4f8cdb4d7c3131477737c5ab9
Tarek Islam <bayridge4th@gmail.com>
2019-03-02 11:37:59 -0500

    Revert "Home"

--- src/components/homecomponents/Feedbar.js
+++ src/components/homecomponents/Feedbar.js
@@ -1,47 +1,21 @@
 import React from 'react'
-import InfiniteScroll from 'react-infinite-scroller'
 
 const Feedbar = (props) => {
-    const { feedName, videoInfo } = props.feed
+    console.log(props.feed.feedName)
+    console.log(props.feed.videoInfo)
     return <>
-    <div>
-        {feedName}
-        <div className='homeRow'>
-        <div style={{"height":"700px", "overflow":"auto"}}>
-    <InfiniteScroll
-        pageStart={1}
-        loadMore={()=>props.loadMoreVideos(props.value)}
-        hasMore={true || false}
-        loader={<div className="loader" key={0}>Loading ...</div>}
-        useWindow={false}
-    >
-    <div className='homeRow'>
-        {videoInfo.map((e,i)=>{
+        <div>{props.feed.feedName}</div>
+        <div>{props.feed.videoInfo.map((e, i)=>{
             return <>
-            <div className='homeCol-3'  key={i}>
-                <div className='homeRow' >
-                    <div className='homeCol-12'>
-                        <img style={{'width':'100%','height':'calc(width*3/4)'}} src={`https://i.ytimg.com/vi/${e.id}/mqdefault.jpg`} alt='' onClick={()=>props.goToVideoPage(e.id)}></img>
-                    </div>
-                    <div className='homeCol-12'>
-                        {e.title}
-                    </div>
-                    <div className='homeCol-12'>
-                        {e.channelName}
-                    </div>
-                    <div className='homeCol-12'>
-                        {e.timePosted}
-                    </div>
-                </div>
+            <div value={e.id} onClick={props.getVideoID} style={{'border':'solid 1px black'}}>
+                <img src={e.photo} key={i}></img>
+                <span>lol</span>
+                <span>{e.title}</span>
+                <span>{e.channelName}</span>
+                <span>{e.timePost}</span>
             </div>
             </>
-        })}
-        </div>
-    </InfiniteScroll>
-</div>
-        </div>
-    </div>
-   
+        })}</div>
     </>
 }
 
--- src/components/homecomponents/Feedlist.js
+++ src/components/homecomponents/Feedlist.js
@@ -2,18 +2,9 @@ import React from 'react'
 
 const Feedlist = (props) => {
     return <>
-        <div className='homeRow' style={{'justifyContent':'center'}}>
-        <div className='homeCol-12'>
-        <span>Feed List</span>
-        </div>
-            {props.feed.map(e=>{
-                return <>
-                    <div className='homeCol-12'>
-                        {e.feedName}
-                    </div>
-                </>
-            })}
-        </div>
+    {/* {props.feed.map((e, i)=>{
+        return e.feedName
+    })} */}
     </>
 }
 
